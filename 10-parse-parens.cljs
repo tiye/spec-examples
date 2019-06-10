2ccbb2b0be52af2ac5d137e478b824190041686a
jiyinyiyong <jiyinyiyong@gmail.com>
2019-06-11 02:42:14 +0800

    parsing S-expressions recursively

--- 10-parse-parens.cljs
+++ 10-parse-parens.cljs
@@ -1,27 +1,25 @@
 
 (ns example.core
  (:require [cljs.spec.alpha :as s]
+           [clojure.string :as string]
            [expound.alpha :refer [expound]]))
 
-(s/def ::left-paren #{"("})
+(s/def ::left-paren #(= % "("))
 
-(s/def ::right-paren #{")"})
+(s/def ::right-paren #(= % ")"))
 
-(s/def ::space (s/and string? (s/conformer seq) (s/+ #{" "})))
+(s/def ::space (fn [x] (and (string? x) (pos? (count x)) (string/blank? x))))
 
-(s/def ::token (s/and string? (s/conformer seq) (s/+ #{"a" "b" "c"})))
+(s/def ::token (fn [x] (contains? #{"a" "b" "c"} x)))
 
 (s/def
  ::example
  (s/cat
-  :left-paren
-  ::left-paren
-  :expr
-  (s/+ (s/or :token ::token :space ::space))
-  :right-paren
-  ::right-paren))
+  :left-paren ::left-paren
+  :expr (s/+ (s/alt :token ::token :space ::space :child ::example))
+  :right-paren ::right-paren))
 
-(let [data (seq "(a b)")]
+(let [data (seq "(aa b (a (b)))")]
   (if (s/valid? ::example data)
     (println (pr-str (s/conform ::example data)))
     (println (s/explain ::example data))))
